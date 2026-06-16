6336fc6e7fd61ff484fbb4e9776dd60a1fbf5a55
HazelSharmaCoderHZ <sharmahazel310@gmail.com>
2026-06-17 00:21:42 +0530

    auth included

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from app.routes.auth import router as auth_router
+
+app.include_router(auth_router)
+
 @app.get("/")
 def home():
-    return {
-        "message": "Backend is alive"
-    }
+    return {"message": "alive"}
