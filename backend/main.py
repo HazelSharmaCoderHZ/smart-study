a807a48cf0dec8bc85e2b4e5ef1427807e30019c
HazelSharmaCoderHZ <sharmahazel310@gmail.com>
2026-06-17 14:52:25 +0530

    error

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,67 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.routes.chat import router as chat_router
-from app.routes.pdf import router as pdf_router
-from app.routes.study import router as study_router
+from app.routes import study, chat
 from app.routes.auth import router as auth_router
+from app.routes.pdf import router as pdf_router
+from app.config.database import client
 
-app = FastAPI()
+app = FastAPI(
+    title="Smart Study Companion API",
+    version="1.0.0"
+)
 
-app.include_router(chat_router, prefix="/api/chat", tags=["Chat"])
-app.include_router(pdf_router, prefix="/api/pdf", tags=["PDF"])
-app.include_router(study_router, prefix="/api/study", tags=["Study"])
+origins = [
+    "http://localhost:3000",
+    "https://smart-study-umber.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    study.router,
+    prefix="/api/study",
+    tags=["Study"]
+)
+
+app.include_router(
+    chat.router,
+    prefix="/api/chat",
+    tags=["Chat"]
+)
+
 app.include_router(auth_router)
+
+app.include_router(
+    pdf_router,
+    prefix="/api/pdf",
+    tags=["PDF"]
+)
 
 @app.get("/")
 def home():
-    return {"message": "working"}
+    return {
+        "message": "Smart Study Companion Backend Running"
+    }
+
+@app.get("/test-db")
+def test_database():
+    try:
+        client.admin.command("ping")
+
+        return {
+            "status": "success",
+            "message": "MongoDB connected successfully"
+        }
+
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
