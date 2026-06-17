4b05b50ef9af478959857ae192db748c270f54f3
HazelSharmaCoderHZ <sharmahazel310@gmail.com>
2026-06-17 14:07:28 +0530

    working auth

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
+
 from app.routes.chat import router as chat_router
 from app.routes.pdf import router as pdf_router
 from app.routes.study import router as study_router
+from app.routes.auth import router as auth_router
+
 app = FastAPI()
 
-app.include_router(
-    chat_router,
-    prefix="/api/chat",
-    tags=["Chat"]
-)
-
-app.include_router(
-    pdf_router,
-    prefix="/api/pdf",
-    tags=["PDF"]
-)
+app.include_router(chat_router, prefix="/api/chat", tags=["Chat"])
+app.include_router(pdf_router, prefix="/api/pdf", tags=["PDF"])
+app.include_router(study_router, prefix="/api/study", tags=["Study"])
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
